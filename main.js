11e6d27fce6138f53dfff9098535f5e39c46f896
roman.vorontsov <cflatminor92@gmail.com>
2023-06-12 00:24:17 +0200

    wip

--- main.js
+++ main.js
@@ -12,18 +12,9 @@ function addListeners() {
 
 function initSlider() {
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        // direction: 'vertical',
         loop: false,
-
-        // If we need pagination
-        // pagination: {
-        //     el: '.swiper-pagination',
-        // },
-        // autoHeight: true,
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 40,
-        // Responsive breakpoints
         breakpoints: {
             // when window width is >= 320px
             // 320: {
@@ -36,24 +27,18 @@ function initSlider() {
             //     spaceBetween: 30
             // },
             // // when window width is >= 640px
-            // 640: {
-            //     slidesPerView: 4,
-            //     spaceBetween: 40
-            // }
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 40
+            }
         },
         autoplay: {
             delay: 2000,
         },
-        // Navigation arrows
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        // scrollbar: {
-        //     el: '.swiper-scrollbar',
-        // },
+        }
     });
 }
 
